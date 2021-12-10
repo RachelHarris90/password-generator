d0c6481d010c56bcbea5f9ca57abd9fea7dc1422
Rachel Harris <rachel.harris@msn.com>
2021-12-11 10:25:28 +1100

    Plan out code for password generator

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+// Prompt for criteria of password
+
+
+// Validate length entered is between 8 and 128 
+
+
+// Capture criteria of password (case, numeric, special character)
+
+
+
+//Randomly generate password 
+//TODO: Use criteria to generate password
+var password = Math.floor((1 + Math.random()) * 0x10000)
+  .toString(16)
+  .substring(1);
+
+console.log(password)
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -9,6 +31,3 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
